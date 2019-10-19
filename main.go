4d396f08eafb897e577d3233ce4acc3e49febece
John Arokiasamy <john@exodus313.local>
2019-10-18 20:16:15 -0500

    rough out

--- main.go
+++ main.go
@@ -3,27 +3,35 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/zserge/webview"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("Incorrect number of parameters, usage: program url\n")
+	if len(os.Args) != 4 {
+		log.Printf("Incorrect number of parameters, usage: program url width height\n")
 		os.Exit(1)
 	}
+
+	width, _ := strconv.ParseInt(os.Args[2], 10, 64)
+	height, _ := strconv.ParseInt(os.Args[3], 10, 64)
 
 	w := webview.New(webview.Settings{
 		Debug:                  true,
 		ExternalInvokeCallback: nil,
-		Width:                  800,
-		Height:                 600,
+		Width:                  int(width),
+		Height:                 int(height),
 		Resizable:              true,
 		Title:                  "Dashboard",
 		URL:                    os.Args[1],
 	})
 
-	w.SetFullscreen(true)
+	if strings.ToLower(os.Getenv("DASHBOARD_DISABLE_FULLSCREEN")) != "y" && strings.ToLower(os.Getenv("DASHBOARD_DISABLE_FULLSCREEN")) != "yes" {
+		w.SetFullscreen(true)
+	}
+
 	log.Printf("Starting for url: %v\n", os.Args[1])
 	w.Run()
 }
